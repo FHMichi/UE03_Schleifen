d92294ab9fd3ac5a33752e3f065c543234d72c42
Michael Mayer <michael.mayer1@edu.fh-joanneum.at>
2024-10-25 14:05:24 +0200

    For Push

--- UE03_Schleifen/Form1.Designer.cs
+++ UE03_Schleifen/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(45, 68);
+            button1.Name = "button1";
+            button1.Size = new Size(338, 74);
+            button1.TabIndex = 0;
+            button1.Text = "Count loop";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.875F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(412, 75);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 50);
+            label1.TabIndex = 1;
+            label1.Text = "?";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1429, 789);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
     }
 }
--- UE03_Schleifen/Form1.cs
+++ UE03_Schleifen/Form1.cs
@@ -6,5 +6,15 @@ namespace UE03_Schleifen
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //"Zähl" Schleife --> For-Loop, Perfekt für Loops in denen gezählt werden muss 
+            label1.Text = string.Empty; 
+            for (int i = 0; i < 10; i++)
+            {
+                label1.Text += i.ToString("00") + "\n"; 
+            }
+        }
     }
 }
